aab6d3d50061e99d1ad84f82d2f4dcae7dfe9b25
Alexandr <sanyad1sna@gmail.com>
2020-03-11 15:11:33 +0300

    added tornado server

--- js/req.js
+++ js/req.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', async (event) => {
     let dataField = document.getElementById('dataField');
     let button = document.getElementById('showData');
-    let response = await fetch("https://api.github.com/users/gaearon");
+    let response = await fetch("http://localhost:8888/");
     let data = await response.json()
     button.addEventListener('click', () => {
-        dataField.innerText = data.login;
+        dataField.innerText = data.data;
     });
 });
---
+++ server/start.py
@@ -0,0 +1,21 @@
+import tornado.ioloop
+import tornado.web
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        response = {
+            'data': 'Congratulations! You\'ve catched data from Tornado Web Server:)'}
+        self.write(response)
+
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
